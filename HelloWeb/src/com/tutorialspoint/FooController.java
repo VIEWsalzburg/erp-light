fb4ca883adc3d38fb49491969a97ee97177b7d9e
Stephan Stadlmair <sstadlmair.its-b2012@fh-salzburg.ac.at>
2014-11-06 18:47:13 +0100

    Commit of backend files

---
+++ HelloWeb/src/com/tutorialspoint/FooController.java
@@ -0,0 +1,20 @@
+package com.tutorialspoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController{
+   
+   @RequestMapping(value="/postListener")
+   public Greeting postListener(HttpServletRequest request, 
+	        HttpServletResponse response)
+   {
+	   System.out.println(request.getSession().getId());
+	   return new Greeting(11,"foo");
+   }
+
+}
