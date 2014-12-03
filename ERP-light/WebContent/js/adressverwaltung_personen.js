f57ca57a7325afa8de48804e4c54618569374466
fhs33400 <dbungarten.its-b2011@fh-salzburg.ac.at>
2014-12-03 14:26:04 +0100

    Javascript, Modals update

--- ERP-light/WebContent/js/adressverwaltung_personen.js
+++ ERP-light/WebContent/js/adressverwaltung_personen.js
@@ -1,20 +1,49 @@
 
-		var telnumelement_template = "<div class='row'> 		<div class='form-group'>															 			<div class='col-sm-5'> 				<input type='text' class='form-control' id='tbx_telnr2' 					placeholder='Telefonnr.'> 			</div> 			<div class='col-sm-4'> 				<select class='form-control'> 					<option>Privat</option> 					<option>Gesch&auml;ftlich</option> 				</select> 			</div> 			<div class='col-sm-3'> 				<button type='button' class='btn btn-default removephonenumber_btn' 					id='btn_delete' >L&ouml;schen</button> 			</div> 		</div> 	</div>";
+		var telnumelement_template = "<div class='row'> 		<div class='form-group'>															 			<div class='col-sm-5'> 				<input type='text' class='form-control tbx_telnr' 					placeholder='Telefonnr.'> 			</div> 			<div class='col-sm-4'> 				<select class='form-control'> 					<option>Privat</option> 					<option>Gesch&auml;ftlich</option> 				</select> 			</div> 			<div class='col-sm-3'> 				<button type='button' class='btn btn-default removephonenumber_btn' 					id='btn_delete' >L&ouml;schen</button> 			</div> 		</div> 	</div>";
 		var telnumCount=0;
 		
+		var mailadress_template = "<div class='row'> 		<div class='form-group'>															 			<div class='col-sm-5'> 				<input type='text' class='form-control tbx_mailadress'					placeholder='Email'> 			</div> 			<div class='col-sm-4'> 				<select class='form-control'> 					<option>Privat</option> 					<option>Gesch&auml;ftlich</option> 				</select> 			</div> 			<div class='col-sm-3'> 				<button type='button' class='btn btn-default removemailadress_btn' 					id='btn_delete' >L&ouml;schen</button> 			</div> 		</div> 	</div>";
+		var mailadressCount=0;
+		
 		$("#pageheader").load("../partials/header.html");
-		$("#adressverwaltung_nav").addClass("active");
-	
+		$("#adressverwaltung_nav").addClass("active");	
 
 	
 		$("#neuanlegen_btn").click(function() {
-			$("#modal_title_text").text("Neue Person");
+			$("#modal_title_text").text("Neue Person");			
 		});
 
+		//Modal Neu anlegen -> speichern
+		$("#saveperson_btn").click(function() {
+			var newperson = new Object();
+			newperson.personId = -1;
+			newperson.salutation = $("#tbx_salutation").val();
+			newperson.title = $("#tbx_title").val();
+			newperson.firstName = $("#tbx_firstName").val();
+			newperson.LastName = $("#tbx_lastName").val();
+			newperson.address = $("#tbx_address").val();
+			newperson.zip = $("#tbx_zip").val();
+			newperson.city = $("#tbx_city").val();
+			newperson.country = $("#tbx_country").val();
+			
+			$(".tbx_telnr").each(function() {
+				newperson.phoneNumbers.push($(this).val());
+			});
+			
+			$(".tbx_mailadress").each(function() {
+				newperson.emails.push($(this).val());
+			});
+			
+			newperson.loginEmail = $("#select_loginEmail").val();
+			newperson.types = $("#select_types").val();
+			newperson.permission = $("#select_permission").val();
+		});
+		
 		$("#edit").click(function() {
 			$("#modal_title_text").text("Bearbeite Person");
 		});
 	
+		//Phonenumber handler
 		$("body").on('click', '.removephonenumber_btn',function() {
 			$(this).closest('div[class^="telnum-element"]').remove();
 				});
@@ -25,13 +54,26 @@
 							$("#addphonenumber_btn")
 									.click(
 											function() {
-												
 												var newElement = $("<div/>",{id:"telnum-elemt"+telnumCount++, "class":"telnum-element"}).append(telnumelement_template);
-												
 												$("#telnum_container").append(newElement);
-																								 
 											});
 						});
+		
+		//Mailadress handler
+		$("body").on('click', '.removemailadress_btn',function() {
+			$(this).closest('div[class^="mailadress-element"]').remove();
+				});
+		
+		$(document)
+		.ready(
+				function() {
+					$("#addmailadress_btn")
+							.click(
+									function() {										
+										var newElement = $("<div/>",{id:"mailadress-element"+mailadressCount++, "class":"mailadress-element"}).append(mailadress_template);
+										$("#mailadress_container").append(newElement);
+									});
+				});
 	
 
 //	<!-- Filterfunktion -->
