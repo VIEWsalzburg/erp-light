33602017e924e98871e683ada8f2a6b5bc7848cf
fhs36084 <mschnoell.its-b2012@fh-salzburg.ac.at>
2014-12-04 17:11:17 +0100

    integrated DataBaseService into ERP-light

---
+++ ERP-light/src/at/erp/light/view/services/DataBaseService.java
@@ -0,0 +1,477 @@
+package at.erp.light.view.services;
+import java.util.List;
+import java.util.Set;
+
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.erp.light.view.model.Address;
+import at.erp.light.view.model.Article;
+import at.erp.light.view.model.Category;
+import at.erp.light.view.model.City;
+import at.erp.light.view.model.Country;
+import at.erp.light.view.model.DeliveryList;
+import at.erp.light.view.model.Email;
+import at.erp.light.view.model.IncomingArticle;
+import at.erp.light.view.model.IncomingDelivery;
+import at.erp.light.view.model.Organisation;
+import at.erp.light.view.model.OutgoingArticle;
+import at.erp.light.view.model.OutgoingDelivery;
+import at.erp.light.view.model.Person;
+import at.erp.light.view.model.Telephone;
+import at.erp.light.view.model.Type;
+
+
+public class DataBaseService implements IDataBase {
+
+	private SessionFactory sessionFactory;
+		
+	public DataBaseService(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+		
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Person getPersonById(int id) {				
+		Query query = sessionFactory.getCurrentSession().createQuery("FROM Person p WHERE p.personId = :id");
+		query.setParameter("id", id);
+		Person person = (Person)query.uniqueResult();
+		return person;
+	}
+	
+	@Override
+	public int setIncomingDelivery(IncomingDelivery incomingDelivery) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Transactional(propagation=Propagation.REQUIRED)
+	@Override
+	public Person getPersonById(int id, int FetchFlags) {
+		Query query = sessionFactory.getCurrentSession().createQuery("FROM Person p WHERE p.personId = :id");
+		query.setParameter("id", id);
+		Person person = (Person)query.uniqueResult();
+		
+		if ( (FetchFlags&Person.FETCH_ADDRESS)!=0)
+		{
+			Hibernate.initialize(person.getAddress());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_CITY)!=0)
+		{
+			Hibernate.initialize(person.getCity());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_COUNTRY)!=0)
+		{
+			Hibernate.initialize(person.getCountry());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_PLATFORMUSER)!=0)
+		{
+			Hibernate.initialize(person.getPlatformuser());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_TYPES)!=0)
+		{
+			Hibernate.initialize(person.getTypes());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_EMAILS)!=0)
+		{
+			Hibernate.initialize(person.getEmails());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_TELEPHONES)!=0)
+		{
+			Hibernate.initialize(person.getTelephones());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_UPDATED_BY_PERSON)!=0)
+		{
+			Hibernate.initialize(person.getPerson());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_ORGANISATION_CONTACT)!=0)
+		{
+			Hibernate.initialize(person.getOrganisation());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_ORGANISATIONS_UPDATED)!=0)
+		{
+			Hibernate.initialize(person.getOrganisations());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_OUTGOINGDELIVERIES_UPDATED)!=0)
+		{
+			Hibernate.initialize(person.getOutgoingDeliveries());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_INCOMINGDELIVERIES_UPDATED)!=0)
+		{
+			Hibernate.initialize(person.getIncomingDeliveries());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_PERSONS_UPDATED)!=0)
+		{
+			Hibernate.initialize(person.getPersons());
+		}
+		
+		if ( (FetchFlags&Person.FETCH_DELIVERYLISTS_UPDATED)!=0)
+		{
+			Hibernate.initialize(person.getDeliveryLists());
+		}
+		
+		return person;
+	}
+
+	@Override
+	public List<Person> getPersonsByType(Type type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public List<Person> getAllPersons() {
+		@SuppressWarnings("unchecked")
+		List<Person> persons = sessionFactory.getCurrentSession()
+			.createQuery("FROM Person p LEFT JOIN fetch p.address LEFT JOIN fetch p.city LEFT JOIN fetch p.country").list();
+		return persons;
+	}
+
+	@Override
+	public Person getPersonByLoginEmail(String loginEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	// setCountry
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Country getCountryByCountry(String country) {
+		Country mCountry = (Country) sessionFactory.getCurrentSession().
+				createQuery("FROM Country c WHERE c.country = :country").
+				setParameter("country", country).uniqueResult();
+		if (mCountry==null)
+		{
+			mCountry = new Country(0, country);
+			setCountry(mCountry);
+		}
+		return mCountry;
+	}
+	
+	// setCountry
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Country setCountry(Country country) {
+		sessionFactory.getCurrentSession().saveOrUpdate(country);
+		return country;
+	}
+	
+	// getAddressByAddress
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Address getAddressByAddress(String address) {
+		Address mAddress = (Address) sessionFactory.getCurrentSession().
+				createQuery("FROM Address a WHERE a.address = :address").
+				setParameter("address", address).uniqueResult();
+		if (mAddress==null)
+		{
+			mAddress = new Address(0, address);
+			setAddress(mAddress);
+		}
+		return mAddress;
+	}
+	
+	// setAddress
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Address setAddress(Address address) {
+		sessionFactory.getCurrentSession().saveOrUpdate(address);
+		return address;
+	}
+	
+	// setCity
+	@Transactional(propagation=Propagation.REQUIRED)
+	public City getCityByCityAndZip(String city, String zip) {
+		City mCity = (City) sessionFactory.getCurrentSession().
+				createQuery("FROM City c WHERE c.city = :city AND c.zip = :zip").
+				setParameter("city", city).setParameter("zip", zip).uniqueResult();
+		if (mCity==null)
+		{
+			mCity = new City(0, city, zip);
+			setCity(mCity);
+		}
+		return mCity;
+	}
+	
+	// setCity
+	@Transactional(propagation=Propagation.REQUIRED)
+	public City setCity(City city) {
+		sessionFactory.getCurrentSession().saveOrUpdate(city);
+		return city;
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public int setPerson(Person person) {
+		
+		// update Address
+		// if address == null => delete FK
+		if (person.getAddress() != null)
+		{
+			// checks if Address already exists in database (if yes => retrieve existing one, if no => create new one and get it)
+			Address Address = getAddressByAddress(person.getAddress().getAddress());
+			person.setAddress(Address);
+		}
+		
+		// update Country
+		if (person.getCountry() != null)
+		{
+			// checks if Country already exists in database (if yes => retrieve existing one, if no => create new one and get it)
+			Country country = getCountryByCountry(person.getCountry().getCountry());
+			person.setCountry(country);
+		}
+		
+		// update City
+		if (person.getCity() != null)
+		{
+			// checks if City and Zip already exist in database (if yes => retrieve existing one, if no => create new one and get it)
+			City city = getCityByCityAndZip(person.getCity().getCity(), person.getCity().getZip());
+			person.setCity(city);
+		}
+		
+		
+		
+		sessionFactory.getCurrentSession().saveOrUpdate(person);
+		
+		// rollback test
+		if (1==0)
+			throw(new HibernateException("rollback that shit! ^^"));
+		
+		return person.getPersonId();
+		
+	}
+
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Type getTypeById(int id) {
+		Query query = sessionFactory.getCurrentSession().createQuery("FROM Type t WHERE t.typeId = :id");
+		query.setParameter("id", id);
+		Type type = (Type)query.uniqueResult();
+		return type;
+	}
+	
+	@Override
+	@Transactional
+	public int telephoneTest() {
+	
+		Person mPerson = getPersonById(36);
+		
+		mPerson.setActive(1);
+		
+		Telephone tel = new Telephone(0, getTypeById(Type.PRIVAT), "00 43 5678");
+		sessionFactory.getCurrentSession().saveOrUpdate(tel);
+		
+		Email email = new Email(0, getTypeById(Type.GESCHÄFTLICH), "mschnoell@hotmail.com");
+		sessionFactory.getCurrentSession().saveOrUpdate(email);
+		
+		mPerson.getTelephones().add(tel);
+		mPerson.getEmails().add(email);
+		
+		// mPerson.getTelephones().remove(mPerson.getTelephones().toArray()[0]);
+		
+		// tel.setPerson(mPerson);
+		
+//		Set<Telephone> telephones = (Set<Telephone>) mPerson.getTelephones();
+//		telephones.add(tel);
+//		mPerson.setTelephones(telephones);
+		
+		// sessionFactory.getCurrentSession().saveOrUpdate(mPerson);
+		
+		return 0;
+		
+	}
+	
+	
+	
+	@Override
+	public int setPersons(List<Person> persons) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Organisation getOrganisationById(int id) {
+		Query query = sessionFactory.getCurrentSession().createQuery("FROM Organisation o WHERE o.organisationId = :id");
+		query.setParameter("id", id);
+		Organisation organisation = (Organisation)query.uniqueResult();
+		return organisation;
+	}
+
+	@Override
+	public List<Organisation> getOrganisationsByCategory(Category category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Organisation> getAllOrganisations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setOrganisation(Organisation organisation) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setOrganisations(List<Organisation> organisations) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public IncomingDelivery getIncomingDeliveryById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IncomingDelivery> getAllIncomingDeliveries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setIncomingDeliveries(List<IncomingDelivery> incomingDeliveries) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public IncomingArticle getIncomingArticleById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IncomingArticle> getAllIncomingArticles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setIncomingArticle(IncomingArticle incomingArticle) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setIncomingArticles(List<IncomingArticle> incomingArticles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Article getArticleById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Article getAllArticles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setArticle(Article article) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setArticles(List<Article> articles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public OutgoingArticle getOutgoingArticleById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OutgoingArticle> getAllOutgoingArticles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setOutgoingArticle(OutgoingArticle outgoingArticle) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setOutgoingArticles(List<OutgoingArticle> outgoingArticles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public OutgoingDelivery getOutgoingDeliveryById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OutgoingDelivery> getAllOutgoingDeliveries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setOutgoingDelivery(OutgoingDelivery outgoingDelivery) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setOutgoingDeliveries(List<OutgoingDelivery> outgoingDeliveries) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DeliveryList getDeliveryListById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DeliveryList> getAllDeliveryLists() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setDeliveryList(DeliveryList deliveryList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDeliveryLists(List<DeliveryList> deliveryLists) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ ERP-light/src/at/erp/light/view/services/MockedDataBaseService.java
@@ -0,0 +1,334 @@
+package at.erp.light.view.services;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Hibernate;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.erp.light.view.model.Address;
+import at.erp.light.view.model.Article;
+import at.erp.light.view.model.Category;
+import at.erp.light.view.model.City;
+import at.erp.light.view.model.Country;
+import at.erp.light.view.model.DeliveryList;
+import at.erp.light.view.model.Email;
+import at.erp.light.view.model.IncomingArticle;
+import at.erp.light.view.model.IncomingDelivery;
+import at.erp.light.view.model.Organisation;
+import at.erp.light.view.model.OutgoingArticle;
+import at.erp.light.view.model.OutgoingDelivery;
+import at.erp.light.view.model.Permission;
+import at.erp.light.view.model.Person;
+import at.erp.light.view.model.Platformuser;
+import at.erp.light.view.model.Telephone;
+import at.erp.light.view.model.Type;
+
+
+public class MockedDataBaseService implements IDataBase {
+
+	// private SessionFactory sessionFactory;
+	
+	private List<Person> mockedPersons = new ArrayList<Person>();
+	
+	Type typePrivat = new Type(Type.PRIVAT, "Privat");
+	Permission permissionAdmin = new Permission(1, 1, "Admin");
+		
+	public MockedDataBaseService() {
+		
+		// this.sessionFactory = sessionFactory;
+		
+		// initialize Persons
+		
+		// first Person
+		Person person1 = new Person(1, "Herr", "Dr.", "Seppi", "Huber", "kommt vom Land", new Date(System.currentTimeMillis()), 1); 
+		Address address1 = new Address(1, "Seppis Straﬂe 1");
+		City city1 = new City(1, "Seppis Stadt", "2345");
+		Country country1 = new Country(1, "Seppis Land");
+		
+		Email email1 = new Email(1, typePrivat, "seppi.huber@gmail.com");
+		Telephone telephone1 = new Telephone(1, typePrivat, "0664 1234567");
+		
+		person1.setAddress(address1);
+		person1.setCity(city1);
+		person1.setCountry(country1);
+		person1.getEmails().add(email1);
+		person1.getTelephones().add(telephone1);
+		Platformuser platformUser1 = new Platformuser(permissionAdmin, person1, "admin", "seppi.huber@gmail.com");
+		
+		
+		
+		// second Person
+		Person person2 = new Person(2, "Frau", "Bsc", "Susi", "Mayer", "kommt aus der Stadt", new Date(System.currentTimeMillis()), 1); 
+		Address address2 = new Address(2, "Susis Straﬂe 2");
+		City city2 = new City(2, "Susis Stadt", "3456");
+		Country country2 = new Country(2, "Susis Land");
+		
+		Email email2 = new Email(2, typePrivat, "susi.mayer@gmail.com");
+		Telephone telephone2 = new Telephone(2, typePrivat, "0664 9876543");
+		
+		person2.setAddress(address2);
+		person2.setCity(city2);
+		person2.setCountry(country2);
+		person2.getEmails().add(email2);
+		person2.getTelephones().add(telephone2);
+		Platformuser platformUser2 = new Platformuser(permissionAdmin, person2, "admin", "susi.mayer@gmail.com");
+		
+		
+		
+		
+		
+		// third Person
+		Person person3 = new Person(3, "Herr", "MSc", "Maxi", "Neumann", "unterrichtet an der FH", new Date(System.currentTimeMillis()), 1); 
+		Address address3 = new Address(3, "Maxis Straﬂe 2");
+		City city3 = new City(3, "Maxis Stadt", "4567");
+		Country country3 = new Country(3, "Maxis Land");
+		
+		Email email3 = new Email(3, typePrivat, "maxi.neumann@gmail.com");
+		Telephone telephone3 = new Telephone(1, typePrivat, "0664 5463728");
+		
+		person3.setAddress(address3);
+		person3.setCity(city3);
+		person3.setCountry(country3);
+		person3.getEmails().add(email3);
+		person3.getTelephones().add(telephone3);
+		Platformuser platformUser3 = new Platformuser(permissionAdmin, person3, "admin", "maxi.neumann@gmail.com");
+		
+		mockedPersons.add(person1);
+		mockedPersons.add(person2);
+		mockedPersons.add(person3);
+		
+	}
+	
+		
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Person getPersonById(int id) {				
+		return mockedPersons.get(0);
+	}
+	
+	@Override
+	public int setIncomingDelivery(IncomingDelivery incomingDelivery) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Transactional(propagation=Propagation.REQUIRED)
+	@Override
+	public Person getPersonById(int id, int FetchFlags) {
+		return mockedPersons.get(0);
+	}
+
+	@Override
+	public List<Person> getPersonsByType(Type type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public List<Person> getAllPersons() {
+		return mockedPersons;
+	}
+
+	@Override
+	public Person getPersonByLoginEmail(String loginEmail) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public int setPerson(Person person) {
+		mockedPersons.add(person);
+		return person.getPersonId();
+		
+	}
+
+	@Override
+	public int setPersons(List<Person> persons) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public Organisation getOrganisationById(int id) {
+		return null;
+	}
+
+	@Override
+	public List<Organisation> getOrganisationsByCategory(Category category) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Organisation> getAllOrganisations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setOrganisation(Organisation organisation) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setOrganisations(List<Organisation> organisations) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public IncomingDelivery getIncomingDeliveryById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IncomingDelivery> getAllIncomingDeliveries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setIncomingDeliveries(List<IncomingDelivery> incomingDeliveries) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public IncomingArticle getIncomingArticleById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<IncomingArticle> getAllIncomingArticles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setIncomingArticle(IncomingArticle incomingArticle) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setIncomingArticles(List<IncomingArticle> incomingArticles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Article getArticleById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Article getAllArticles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setArticle(Article article) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setArticles(List<Article> articles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public OutgoingArticle getOutgoingArticleById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OutgoingArticle> getAllOutgoingArticles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setOutgoingArticle(OutgoingArticle outgoingArticle) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setOutgoingArticles(List<OutgoingArticle> outgoingArticles) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public OutgoingDelivery getOutgoingDeliveryById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<OutgoingDelivery> getAllOutgoingDeliveries() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setOutgoingDelivery(OutgoingDelivery outgoingDelivery) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setOutgoingDeliveries(List<OutgoingDelivery> outgoingDeliveries) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public DeliveryList getDeliveryListById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DeliveryList> getAllDeliveryLists() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int setDeliveryList(DeliveryList deliveryList) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDeliveryLists(List<DeliveryList> deliveryLists) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public int telephoneTest() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
