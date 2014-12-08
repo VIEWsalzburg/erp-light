96815dde5cca61993329a5b75a375867cc6bd4b7
fhs33400 <dbungarten.its-b2011@fh-salzburg.ac.at>
2014-12-08 15:22:21 +0100

    "My data" modal added.

--- ERP-light/WebContent/js/header.js
+++ ERP-light/WebContent/js/header.js
@@ -4,48 +4,49 @@ var phoneelement_template = "";
 var emailCount = 0;
 var emailelement_template = "";
 
+alert("test");
+
 // Get one person and load it to modal
 $(document).ready(function() {
 	//remove all phonenumber divs
-//	$(".btn_removephonenumber").closest('div[class^="phone_element"]').remove();
-//	phoneCount = 0;
+	$(".btn_removephonenumber").closest('div[class^="phone_element"]').remove();
+	phoneCount = 0;
 	
 	//remove all email divs
-//	$(".btn_removeemail").closest('div[class^="email_element"]').remove();
-//	emailCount = 0;
+	$(".btn_removeemail").closest('div[class^="email_element"]').remove();
+	emailCount = 0;
 	
 	//get current user
 	$.ajax({
 		type : "POST",
-		url : "../rest/secure/person/getCurrentUser" //TODO not working
+		url : "../rest/secure/person/getCurrentUser"
 	}).done(function(data) {
 
 		var p = eval(data);
 
 		//load data to modal
-		$("#tbx_id").val(p.personId);
-		$("#tbx_salutation").val(p.salutation);
-		$("#tbx_title").val(p.title);
-		$("#tbx_firstName").val(p.firstName);
-		$("#tbx_lastName").val(p.lastName);
-		$("#tbx_address").val(p.address);
-		$("#tbx_zip").val(p.zip);
-		$("#tbx_city").val(p.city);
-		$("#tbx_country").val(p.country);
+		$("#tbx_id_mydata").val(p.personId);
+		$("#tbx_salutation_mydata").val(p.salutation);
+		$("#tbx_title_mydata").val(p.title);
+		$("#tbx_firstName_mydata").val(p.firstName);
+		$("#tbx_lastName_mydata").val(p.lastName);
+		$("#tbx_address_mydata").val(p.address);
+		$("#tbx_zip_mydata").val(p.zip);
+		$("#tbx_city_mydata").val(p.city);
+		$("#tbx_country_mydata").val(p.country);
 
 		//load phoneNumber divs
 		var newElement;
 		var loginEmail_template;
 		var help;
 		var help1;
-		$("#select_loginEmail").find('option')
-	    .remove()
-	    .end();
+		$("#select_loginEmail").find('option').remove().end();
+		
 		for (var i = 0; i<p.telephones.length; i++) {
-			phoneelement_template = "<div class='row'> <div class='form-group'> <div class='col-sm-5'> <input type='text' id='tbx_phoneNumber" 
-				+ phoneCount + "' class='form-control tbx_phoneNumber' placeholder='Telefonnr.'> </div> <div class='col-sm-4'>" +
-				"<select class='form-control' id='select_phoneNumber"+ phoneCount +"'> <option>privat</option> <option>gesch&auml;ftlich</option> </select>" +
-				"</div> <div class='col-sm-3'> <button type='button' class='btn btn-danger btn_removephonenumber' id='btn_delete' >L&ouml;schen</button> </div> </div> </div>";
+			phoneelement_template = "<div class='row'> <div class='form-group'> <div class='col-sm-5'> <input type='text' id='tbx_phoneNumber_mydata" 
+				+ phoneCount + "' class='form-control tbx_phoneNumber_mydata' placeholder='Telefonnr.'> </div> <div class='col-sm-4'>" +
+				"<select class='form-control' id='select_phoneNumber_mydata"+ phoneCount +"'> <option>privat</option> <option>gesch&auml;ftlich</option> </select>" +
+				"</div> <div class='col-sm-3'> <button type='button' class='btn btn-danger btn_removephonenumber' id='btn_delete_mydata' >L&ouml;schen</button> </div> </div> </div>";
 				
 				newElement = $(
 					"<div/>",
@@ -53,13 +54,13 @@ $(document).ready(function() {
 						id : "phone_element" + phoneCount++,
 						"class" : "phone_element"
 					}).append(phoneelement_template);
-					$("#phone_container").append(newElement);
+					$("#phone_container_mydata").append(newElement);
 			
-			help = "#tbx_phoneNumber" + (phoneCount-1);
+			help = "#tbx_phoneNumber_mydata" + (phoneCount-1);
 			$(help).val(p.telephones[i]);
 			
 			help = 'geschäftlich'; //test
-			help1 = "select#select_phoneNumber" + (phoneCount-1) + " option";
+			help1 = "select#select_phoneNumber_mydata" + (phoneCount-1) + " option";
 			$(help1).each(function() { 
 				this.selected = (this.text == help);
 			});
@@ -67,22 +68,22 @@ $(document).ready(function() {
 		
 		//load email divs
 		for (var i = 0; i<p.emails.length; i++) {
-			emailelement_template = "<div class='row'> <div class='form-group'> <div class='col-sm-5'> <input type='text' id='tbx_email" + emailCount + "' " +
-				"class='form-control tbx_mailadress' placeholder='Email'> </div> <div class='col-sm-4'> <select class='form-control' id='select_email"+ emailCount +"'>" +
+			emailelement_template = "<div class='row'> <div class='form-group'> <div class='col-sm-5'> <input type='text' id='tbx_email_mydata" + emailCount + "' " +
+				"class='form-control tbx_mailadress_mydata' placeholder='Email'> </div> <div class='col-sm-4'> <select class='form-control' id='select_email_mydata"+ emailCount +"'>" +
 				"<option>privat</option> <option>gesch&auml;ftlich</option> </select> </div> <div class='col-sm-3'><button type='button' class='btn btn-danger btn_removeemail'" +
-				"id='btn_delete' >L&ouml;schen</button> </div> </div> </div>";
+				"id='btn_delete_mydata' >L&ouml;schen</button> </div> </div> </div>";
 			
 				newElement = $("<div/>", {
 					id : "email_element" + emailCount++,
 					"class" : "email_element"
 				}).append(emailelement_template);
-				$("#email_container").append(newElement);
+				$("#email_container_mydata").append(newElement);
 				
-			help = "#tbx_email" + (emailCount-1);
+			help = "#tbx_email_mydata" + (emailCount-1);
 			$(help).val(p.emails[i]);
 			
 			help = 'geschäftlich';	//test
-			help1 = "select#select_email" + (emailCount-1) + " option";
+			help1 = "select#select_email_mydata" + (emailCount-1) + " option";
 			$(help1).each(function() { 
 				this.selected = (this.text == help);
 			});
@@ -90,34 +91,71 @@ $(document).ready(function() {
 	});
 });
 
-//Save
+//add phonenumber div
+$(document).ready(function() {
+	$("#btn_addphonenumber_mydata").click(function() {
+			phoneelement_template = "<div class='row'> <div class='form-group'> <div class='col-sm-5'> <input type='text' id='tbx_phoneNumber_mydata" 
+									+ phoneCount + "' class='form-control tbx_phoneNumber_mydata' placeholder='Telefonnr.'> </div> <div class='col-sm-4'>" +
+									"<select class='form-control' id='select_phoneNumber_mydata"+ phoneCount +"'> <option>privat</option> <option>gesch&auml;ftlich</option> </select>" +
+									"</div> <div class='col-sm-3'> <button type='button' class='btn btn-danger btn_removephonenumber' id='btn_delete_mydata' >L&ouml;schen</button> </div> </div> </div>";
+									
+									var newElement = $(
+										"<div/>",
+										{
+											id : "phone_element" + phoneCount++,
+											"class" : "phone_element"
+										}).append(phoneelement_template);
+										$("#phone_container_mydata").append(newElement);
+	});
+});
+
+//add email div
+$(document).ready(function() {
+	$("#btn_addemail_mydata").click(function() {
+		emailelement_template = "<div class='row'> <div class='form-group'> <div class='col-sm-5'> <input type='text' id='tbx_email_mydata" + emailCount + "' " +
+			"class='form-control tbx_mailadress_mydata' placeholder='Email'> </div> <div class='col-sm-4'> <select class='form-control' id='select_email_mydata"+ emailCount +"'>" +
+			"<option>privat</option> <option>gesch&auml;ftlich</option> </select> </div> <div class='col-sm-3'><button type='button' class='btn btn-danger btn_removeemail'" +
+			"id='btn_delete_mydata' >L&ouml;schen</button> </div> </div> </div>";
+		
+		var newElement = $("<div/>", {
+			id : "email_element" + emailCount++,
+			"class" : "email_element"
+		}).append(emailelement_template);
+		$("#email_container_mydata").append(newElement);
+		
+	});
+});
+
+//Save my data
 $("#btn_savemydata").click(function() {
+	
 	var newperson = new Object();
 
-	newperson.personId = $("#tbx_id").val();
-	newperson.salutation = $("#tbx_salutation").val();
-	newperson.title = $("#tbx_title").val();
-	newperson.firstName = $("#tbx_firstName").val();
-	newperson.lastName = $("#tbx_lastName").val();
+	newperson.personId = $("#tbx_id_mydata").val();
+	newperson.salutation = $("#tbx_salutation_mydata").val();
+	newperson.title = $("#tbx_title_mydata").val();
+	newperson.firstName = $("#tbx_firstName_mydata").val();
+	newperson.lastName = $("#tbx_lastName_mydata").val();
 	//Not used yet
 	newperson.comment = "";
 	//Set by server
 	newperson.updateTimestamp = "";
 	newperson.active = 1;
 
-	newperson.address = $("#tbx_address").val();
-	newperson.city = $("#tbx_city").val();
-	newperson.zip = $("#tbx_zip").val();
-	newperson.country = $("#tbx_country").val();
+	newperson.address = $("#tbx_address_mydata").val();
+	newperson.city = $("#tbx_city_mydata").val();
+	newperson.zip = $("#tbx_zip_mydata").val();
+	newperson.country = $("#tbx_country_mydata").val();
 
+	var typesArray = [];
 	newperson.types = typesArray;
 
 	newperson.emails = [];
-	$(".tbx_mailadress").each(function (){
+	$(".tbx_mailadress_mydata").each(function (){
 		newperson.emails.push($(this).val());
 	});
 	newperson.telephones = [];
-	$(".tbx_phoneNumber").each(function (){
+	$(".tbx_phoneNumber_mydata").each(function (){
 		newperson.telephones.push($(this).val());
 	});
 	
@@ -134,15 +172,11 @@ $("#btn_savemydata").click(function() {
 		data : JSON.stringify(newperson)
 	}).done(function(data) {
 		if (data) {
-			$('#personTableBody').empty();
-			$('#new').modal('hide');
-			loadTableContent();
+			$('#mydata').modal('hide');
+			loadTableContentCurrentUser();
 		} else {
 			alert("Verbindungsproblem mit dem Server");
 		}
 	});
 	return false;
 });
-
-
-
