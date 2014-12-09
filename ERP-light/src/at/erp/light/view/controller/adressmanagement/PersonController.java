319c3e37b0cdfee770ebcecff77ae2c951f0abfd
Stephan Stadlmair <sstadlmair.its-b2012@fh-salzburg.ac.at>
2014-12-09 19:23:46 +0100

    Controller for password functionallity and last update added

--- ERP-light/src/at/erp/light/view/controller/adressmanagement/PersonController.java
+++ ERP-light/src/at/erp/light/view/controller/adressmanagement/PersonController.java
@@ -69,61 +69,64 @@ public class PersonController {
 	@RequestMapping(value = "secure/person/getCurrentUser")
 	public PersonDTO getCurrentUser(HttpServletRequest request,
 			HttpServletResponse response) {
-		int personId = (int)request.getSession().getAttribute("id");
-		
+		int personId = (int) request.getSession().getAttribute("id");
+
 		Person currentPerson = dataBaseService.getPersonById(personId);
 		if (currentPerson == null)
 			return null;
 
 		PersonDTO currentPersonDTO = PersonMapper.mapToDTO(currentPerson);
 		return currentPersonDTO;
-		
+
 	}
 
 	// TODO Mapping of letzter bearbeiter
 	@RequestMapping(value = "/secure/person/setPerson")
 	boolean setPerson(@RequestBody PersonDTO person, HttpServletRequest request) {
 		Person entity = PersonMapper.mapToEntity(person);
-		
 		// set current user for updater
-		entity.setPerson(dataBaseService.getPersonById((int)request.getSession().getAttribute("id")));
-		
+		entity.setPerson(dataBaseService.getPersonById((int) request
+				.getSession().getAttribute("id")));
+
 		// persist Person to DB
 		dataBaseService.setPerson(entity);
-		
+
 		boolean isPlatformuser = false;
 		// TODO Check for Platformuser and include Checkbox in GUI
 		if (person.getLoginEmail() != null)
 			if (!person.getLoginEmail().isEmpty())
-			isPlatformuser = true;
-		
-		
-		
-		
-		if (isPlatformuser)
-		{
+				isPlatformuser = true;
+
+		if (isPlatformuser) {
 			System.out.println("is platformuser");
-			Platformuser existingPU = dataBaseService.getPlatformuserById(entity.getPersonId());	// get existing Platformuser
-			Permission mPermission = dataBaseService.getPermissionByPermission(person.getPermission());
-			if (existingPU==null)	// if platformuser does not exist
+			Platformuser existingPU = dataBaseService
+					.getPlatformuserById(entity.getPersonId()); // get existing
+																// Platformuser
+			Permission mPermission = dataBaseService
+					.getPermissionByPermission(person.getPermission());
+			if (existingPU == null) // if platformuser does not exist
 			{
 				// create new one
-				existingPU = new Platformuser(mPermission, entity, "default", person.getLoginEmail());
+				existingPU = new Platformuser(mPermission, entity, "default",
+						person.getLoginEmail());
 				System.out.println("created new one");
-			} else {				// if platformuser exists
+			} else { // if platformuser exists
 				System.out.println("update existing one");
-				existingPU.setPermission(mPermission);				// update permission
-				existingPU.setLoginEmail(person.getLoginEmail());	// update loginEmail
+				existingPU.setPermission(mPermission); // update permission
+				existingPU.setLoginEmail(person.getLoginEmail()); // update
+																	// loginEmail
 				existingPU.setPerson(entity);
 			}
-			dataBaseService.setPlatformuser(existingPU);	// persist new/edited platformuser to DB
-		}
-		else	// else remove platformuser (even call, if no platformuser exists)
+			dataBaseService.setPlatformuser(existingPU); // persist new/edited
+															// platformuser to
+															// DB
+		} else // else remove platformuser (even call, if no platformuser
+				// exists)
 		{
 			System.out.println("removed platformuser");
 			dataBaseService.removePlatformuserById(person.getPersonId());
 		}
-		
+
 		return true;
 	}
 
@@ -141,38 +144,68 @@ public class PersonController {
 
 		dataBaseService.setPersons(returnList);
 
-		return new ControllerMessage(true, "delete successful");
+		return new ControllerMessage(true, "Speichern erfolgreich");
 	}
+
+	@RequestMapping(value = "secure/person/resetPasswordForId/{id}")
+	public ControllerMessage resetPasswordForId(@PathVariable int id)
+			throws IOException {
+		Platformuser platformuser = dataBaseService.getPlatformuserById(id);
+		platformuser.setPassword("default");
+		dataBaseService.setPlatformuser(platformuser);
+		return new ControllerMessage(true, "Zurücksetzen erfolgreich");
+	}
+
+	@RequestMapping(value = "secure/person/changeCurrentUserPassword/")
+	public ControllerMessage changeCurrentUserPassword(
+			HttpServletRequest httpServletRequest,
+			@RequestBody String newPassword) throws IOException {
+
+		Object currentId = httpServletRequest.getAttribute("id");
+		if (currentId == null) {
+			return new ControllerMessage(false, "Nicht eingeloggt");
+		} else {
+			Platformuser platformuser = dataBaseService
+					.getPlatformuserById((int) currentId);
+			platformuser.setPassword(newPassword);
+			dataBaseService.setPlatformuser(platformuser);
+			return new ControllerMessage(true, "Ändern erfolgreich");
+		}
+	}
+
 	@RequestMapping(value = "secure/person/getAllPersonsAsCSV")
 	public void downloadCSV(HttpServletResponse response) throws IOException {
-	
+
 		String csvFileName = "personen.csv";
 		response.setContentType("text/csv");
 		// creates mock data
 		String headerKey = "Content-Disposition";
 		String headerValue = String.format("attachment; filename=\"%s\"",
-		csvFileName);
-		
+				csvFileName);
+
 		response.setHeader(headerKey, headerValue);
 		List<Person> listPersons = dataBaseService.getAllPersons();
 		// uses the Super CSV API to generate CSV data from the model data
 		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(),
-		CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE);
+				CsvPreference.EXCEL_NORTH_EUROPE_PREFERENCE);
 
 		String[] header = { "Personen ID", "Anrede", "Titel", "Vorname",
-		"Nachname", "Kommentar", "Letzte Änderung", "Aktiv", "Adresse", "Stadt", "Postleitzahl", "Land", "Login", "Berechtigung", "Typen" , "Emails", "Tel. Nummern"};
+				"Nachname", "Kommentar", "Letzte Änderung", "Aktiv", "Adresse",
+				"Stadt", "Postleitzahl", "Land", "Login", "Berechtigung",
+				"Typen", "Emails", "Tel. Nummern" };
 
-		String[] objectHeader = { "personId", "salutation", "title", "firstName",
-				"lastName", "comment", "updateTimestamp", "active", "address", "city", "zip", "country", "loginEmail", "permission", "types" , "emails", "telephones"};
+		String[] objectHeader = { "personId", "salutation", "title",
+				"firstName", "lastName", "comment", "updateTimestamp",
+				"active", "address", "city", "zip", "country", "loginEmail",
+				"permission", "types", "emails", "telephones" };
 
-		
 		csvWriter.writeHeader(header);
-		
+
 		for (Person p : listPersons) {
 			csvWriter.write(PersonMapper.mapToDTO(p), objectHeader);
 		}
-		
+
 		csvWriter.close();
 	}
-	
+
 }
