158a17ada3734978121cdc7ed01371ed67801c38
fhs33400 <dbungarten.its-b2011@fh-salzburg.ac.at>
2014-12-09 18:03:27 +0100

    Started working on organisations.

--- ERP-light/WebContent/js/adressverwaltung_organisationen.js
+++ ERP-light/WebContent/js/adressverwaltung_organisationen.js
@@ -3,12 +3,83 @@ $("#pageheader").load("../partials/header.html", function() {
 	$("#adressverwaltung_nav").addClass("active");
 });
 
+//load contact persons to modal
+function loadAllContactPersons() {
+	$.ajax({
+		type : "POST",
+		url : "../rest/secure/person/getAll"
+	}).done(
+			function(data) {
+				var p = eval(data);
+				
+				for (var e in p) {
+					var phoneString = "";
+					var phoneNumbers = p[e].telephones;
+					
+					for (var j = 0; j < phoneNumbers.length; j++) {
+						phoneString = phoneString + phoneNumbers[j].telephone;
+						if (j < phoneNumbers.length - 1) {
+							phoneString = phoneString + ", ";
+						}
+					}
+					
+					var divRow = "<div class='boxElement_person'>" + "<span>" + p[e].personId + " " + p[e].firstName + " " + p[e].lastName
+								+ " " + phoneString + " " + "</span><input type='checkbox' id='cbx_contactperson" + p[e].personId + "'></div>";
+					
+					$("#contactPersonDiv").append(divRow);
+				}
+			});
+};
+
+//load categories to modal (unfinished)
+function loadAllCategories() {
+	$.ajax({
+		type : "POST",
+		url : "../rest/secure/person/getAll"
+	}).done(
+			function(data) {
+				var p = eval(data);
+				
+				for (var e in p) {
+					
+					var divRow = "<div class='boxElement_category'>" + "<span>" + p[e].personId + " " + p[e].firstName  + " " 
+					+ "</span><input type='checkbox' id='cbx_contactperson" + p[e].personId + "'></div>";
+					
+					$("#categoryDiv").append(divRow);
+				}
+			});
+};
+
 $("#btn_new").click(function() {
 	$("#modal_title_text").text("Neue Organisation");
+	
+	//clear textboxes
+	$("#tbx_type").val("");
+	$("#tbx_name").val("");
+	$("#tbx_description").val("");
+	$("#tbx_address").val("");
+	$("#tbx_zip").val("");
+	$("#tbx_city").val("");
+	$("#tbx_country").val("");
+	
+	loadAllContactPersons();
+	//loadAllCategories();
 });
 
 $("#btn_edit").click(function() {
 	$("#modal_title_text").text("Bearbeite Organisation");
+	
+	//load textboxes
+	$("#tbx_type").val(tableData[1]);
+	$("#tbx_name").val(tableData[2]);
+	$("#tbx_description").val(tableData[3]);
+	$("#tbx_address").val(tableData[4]);
+	$("#tbx_zip").val(tableData[5]);
+	$("#tbx_city").val(tableData[6]);
+	$("#tbx_country").val("Österreich");
+	
+	loadAllContactPersons();
+	//loadAllCategories();
 });
 
 //TODO load organisation table
@@ -18,10 +89,12 @@ function loadTableContent() {
 		url : "../rest/secure/person/getAll"
 	}).done(
 			function(data) {
+				
 				var p = eval(data);
 				
 				for (var e in p) {
 					var types = p[e].types;
+					var typeString = "";
 					
 					for (var k = 0; k < types.length; k++) {
 						typeString = typeString + types[k];
@@ -30,26 +103,26 @@ function loadTableContent() {
 						}
 					}
 					
-					var tableRow = "<tr>" + "<td>" + p[e].personId
-							+ "</td>" + "<td>" + p[e].salutation
-							+ "</td>" + "<td>" + p[e].title + "</td>"
-							+ "<td>" + p[e].firstName + "</td>"
-							+ "<td>" + p[e].lastName + "</td>" + "<td>"
-							+ p[e].address + ", " + p[e].zip + " "
-							+ p[e].city + ", " + p[e].country + "</td>"
-							+ "<td>" + emailString + "</td>" + "<td>"
-							+ phoneString + "</td>" + "<td>"
-							+ p[e].updateTimestamp + "</td>" + "<td>"
-							+ p[e].permission + "</td>" + "<td>"
-							+ typeString + "</td>" + "<td>"	+ "</tr>";
-					
+					var tableRow = "<tr>" + 
+								"<td>" + p[e].personId + "</td>" 
+								+ "<td>" + "Lieferant" + "</td>" 
+								+ "<td>" + "Musterfirma" + "</td>"
+								+ "<td>" + "Nimmt kein Brot ab" + "</td>"
+								+ "<td>" + p[e].address + "</td>" 
+								+ "<td>" + p[e].zip + "</td>"
+								+ "<td>" + p[e].city + "</td>"
+								+ "<td>" + p[e].firstName + " " + p[e].lastName + "</td>" 
+								+ "<td>" + "Kategorie1" + "</td>"
+								+ "<td>" + p[e].updateTimestamp + "</td>"
+								+ "<td>" + p[e].firstName + " " + p[e].lastName + "</td>" +	"</tr>";
+							
 					$("#organisationTableBody").append(tableRow);
 				}
 			});
 };
 
 //Get all organisations and load into table
-//$(document).ready(loadTableContent());
+$(document).ready(loadTableContent());
 
 //search filter
 $(document).ready(function() {
