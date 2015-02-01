db78c285ddcff689ab5cd7928e1e9789757d8c72
fhs33400 <dbungarten.its-b2011@fh-salzburg.ac.at>
2015-02-01 16:42:52 +0100

    - mainly implementation of reporting_waren.js (not fully working)

--- ERP-light/WebContent/js/warenverwaltung_neuerwareneingang.js
+++ ERP-light/WebContent/js/warenverwaltung_neuerwareneingang.js
@@ -120,7 +120,7 @@ function loadNewIncomingDelivery(id){
 }
 
 //loads all deliverers
-function loadAllDeliverers(id) {
+function loadAllDeliverers() {
 	$.ajax({
 		type : "POST",
 		async : false,
