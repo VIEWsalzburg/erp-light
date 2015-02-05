6dbaf1a7d2c6240b7e37e90366e5f00bec219dfc
Matthias Schnöll <mschnoell.its-b2012@fh-salzburg.ac.at>
2015-02-05 14:44:19 +0100

    Fix39: * fixed logging info for PersonController

--- ERP-light/src/at/erp/light/view/controller/adressmanagement/PersonController.java
+++ ERP-light/src/at/erp/light/view/controller/adressmanagement/PersonController.java
@@ -5,6 +5,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,7 @@ import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
 import at.erp.light.view.authenticate.HashGenerator;
+import at.erp.light.view.controller.article.DeliveryController;
 import at.erp.light.view.dto.PersonDTO;
 import at.erp.light.view.mapper.PersonMapper;
 import at.erp.light.view.model.Permission;
@@ -30,7 +32,9 @@ import at.erp.light.view.state.ControllerMessage;
 
 @RestController
 public class PersonController {
-
+	private static final Logger log = Logger.getLogger(DeliveryController.class
+			.getName());
+	
 	@Autowired
 	private IDataBase dataBaseService;
 
@@ -45,29 +49,21 @@ public class PersonController {
 		for (Person p : dataBaseService.getAllPersons()) {
 			list.add(PersonMapper.mapToDTO(p));
 		}
+		
+		log.info("returning all Persons");
+		
 		return list;
 	}
 
 	@RequestMapping(value = "secure/person/getPersonById/{id}")
 	public PersonDTO getPersonById(@PathVariable int id) {
-		List<PersonDTO> list = new ArrayList<PersonDTO>();
 
-		for (Person p : dataBaseService.getAllPersons()) {
-			list.add(PersonMapper.mapToDTO(p));
-		}
-
-		int found = -1, i = 0;
-		for (PersonDTO element : list) {
-			if (element.getPersonId() == id) {
-				found = i;
-			}
-			i++;
-		}
-
-		if (found != -1)
-			return list.get(found);
-		else
-			return null;
+		Person p = dataBaseService.getPersonById(id);
+		PersonDTO person = PersonMapper.mapToDTO(p);
+		
+		log.info("returning person with id: "+person.getPersonId());
+		return person;
+		
 	}
 
 	@RequestMapping(value = "secure/person/getCurrentUser")
@@ -80,6 +76,7 @@ public class PersonController {
 			return null;
 
 		PersonDTO currentPersonDTO = PersonMapper.mapToDTO(currentPerson);
+		log.info("returning logged in Person");
 		return currentPersonDTO;
 
 	}
@@ -87,72 +84,86 @@ public class PersonController {
 	@RequestMapping(value = "/secure/person/changeMyData")
 	ControllerMessage changeMyData(@RequestBody PersonDTO person, HttpServletRequest request) {
 		
-		Person myNewPerson = PersonMapper.mapToEntity(person);
-		Person myPerson = dataBaseService.getPersonById((int)request.getSession().getAttribute("id"));
-		
-		myPerson.setSalutation(myNewPerson.getSalutation());
-		myPerson.setTitle(myNewPerson.getTitle());
-		myPerson.setFirstName(myNewPerson.getFirstName());
-		myPerson.setLastName(myNewPerson.getLastName());
-		myPerson.setAddress(myNewPerson.getAddress());
-		myPerson.setCity(myNewPerson.getCity());
-		myPerson.setCountry(myNewPerson.getCountry());
-		myPerson.setTelephones(myNewPerson.getTelephones());
-		myPerson.setEmails(myNewPerson.getEmails());
-		
-		myPerson.setLastEditor(dataBaseService.getPersonById((int) request
-				.getSession().getAttribute("id")));
-		dataBaseService.setPerson(myPerson);
+		try {
+			
+			Person myNewPerson = PersonMapper.mapToEntity(person);
+			int personId = (int)request.getSession().getAttribute("id");
+			Person myPerson = dataBaseService.getPersonById(personId);
+			
+			myPerson.setSalutation(myNewPerson.getSalutation());
+			myPerson.setTitle(myNewPerson.getTitle());
+			myPerson.setFirstName(myNewPerson.getFirstName());
+			myPerson.setLastName(myNewPerson.getLastName());
+			myPerson.setAddress(myNewPerson.getAddress());
+			myPerson.setCity(myNewPerson.getCity());
+			myPerson.setCountry(myNewPerson.getCountry());
+			myPerson.setTelephones(myNewPerson.getTelephones());
+			myPerson.setEmails(myNewPerson.getEmails());
+			
+			myPerson.setLastEditor(dataBaseService.getPersonById(personId));
+			dataBaseService.setPerson(myPerson);
 
+			log.info("Changing MyData successful");
+			return new ControllerMessage(true, "Speichern erfolgreich!");
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			log.info("Changing MyData not successful");
+			return new ControllerMessage(false, "Speichern nicht erfolgreich!");
+		}
 		
-		return new ControllerMessage(true, "Speichern erfolgreich!");
 	}
 	
 	
 	@RequestMapping(value = "/secure/person/setPerson")
 	ControllerMessage setPerson(@RequestBody PersonDTO person, HttpServletRequest request) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-		Person entity = PersonMapper.mapToEntity(person);
+		try {
 		
-		// set current user for updater
-		entity.setLastEditor(dataBaseService.getPersonById((int) request
-				.getSession().getAttribute("id")));
-
-		// persist Person to DB
-		dataBaseService.setPerson(entity);
-
-		boolean isPlatformuser = person.isSystemUser();
-		
-		if (isPlatformuser) {
-			System.out.println("is platformuser");
-			Platformuser existingPU = dataBaseService
-					.getPlatformuserById(entity.getPersonId()); // get existing
-																// Platformuser
-			Permission mPermission = dataBaseService
-					.getPermissionByPermission(person.getPermission());
-			if (existingPU == null) // if platformuser does not exist
+			Person entity = PersonMapper.mapToEntity(person);
+			
+			// set current user for updater
+			entity.setLastEditor(dataBaseService.getPersonById((int) request
+					.getSession().getAttribute("id")));
+	
+			// persist Person to DB
+			dataBaseService.setPerson(entity);
+			log.info("saved person with id: "+entity.getPersonId());
+	
+			boolean isPlatformuser = person.isSystemUser();
+			
+			if (isPlatformuser) {
+				log.info("person is platformUser");
+				Platformuser existingPU = dataBaseService
+						.getPlatformuserById(entity.getPersonId()); // get existing Platformuser
+				Permission mPermission = dataBaseService
+						.getPermissionByPermission(person.getPermission());
+				if (existingPU == null) // if platformuser does not exist
+				{
+					// create new one
+					existingPU = new Platformuser(mPermission, entity, HashGenerator.hashPasswordWithSalt("default"),
+							person.getLoginEmail());
+					log.info("created new platfomUser for person with id: "+entity.getPersonId());
+				} else { // if platformuser exists
+					existingPU.setPermission(mPermission); // update permission
+					existingPU.setLoginEmail(person.getLoginEmail()); // update loginEmail
+					existingPU.setPerson(entity);
+					log.info("updated existing platformUser for person with id: "+entity.getPersonId());
+				}
+				dataBaseService.setPlatformuser(existingPU); // persist new/edited platformuser to DB
+			} else // else remove platformuser (even call, if no platformuser exists)
 			{
-				// create new one
-				existingPU = new Platformuser(mPermission, entity, HashGenerator.hashPasswordWithSalt("default"),
-						person.getLoginEmail());
-				System.out.println("created new one");
-			} else { // if platformuser exists
-				System.out.println("update existing one");
-				existingPU.setPermission(mPermission); // update permission
-				existingPU.setLoginEmail(person.getLoginEmail()); // update
-																	// loginEmail
-				existingPU.setPerson(entity);
+				dataBaseService.removePlatformuserById(person.getPersonId());
+				log.info("no platformUser");
 			}
-			dataBaseService.setPlatformuser(existingPU); // persist new/edited
-															// platformuser to
-															// DB
-		} else // else remove platformuser (even call, if no platformuser
-				// exists)
+	
+			return new ControllerMessage(true, "Speichern erfolgreich!");
+		} catch (Exception e)
 		{
-			System.out.println("removed platformuser");
-			dataBaseService.removePlatformuserById(person.getPersonId());
+			e.printStackTrace();
+			log.severe("failed saving person");
+			return new ControllerMessage(false, "Speichern fehlgeschlagen!");
 		}
-
-		return new ControllerMessage(true, "Speichern erfolgreich!");
+	
 	}
 
 	@RequestMapping(value = "secure/person/deletePersonById/{id}")
@@ -163,9 +174,11 @@ public class PersonController {
 		} catch (HibernateException e)
 		{
 			e.printStackTrace();
+			log.severe("deleting person with id "+id+" failed");
 			return new ControllerMessage(false, "Löschen fehlgeschlagen!");
 		}
 		
+		log.info("deleted person with id: "+id);
 		return new ControllerMessage(true, "Löschen erfolgreich!");
 		
 	}
@@ -173,12 +186,19 @@ public class PersonController {
 	@RequestMapping(value = "secure/person/resetPasswordForId/{id}")
 	public ControllerMessage resetPasswordForId(@PathVariable int id)
 			throws IOException, NoSuchAlgorithmException {
-		Platformuser platformuser = dataBaseService.getPlatformuserById(id);
-		
-		platformuser.setPassword(HashGenerator.hashPasswordWithSalt("default"));
-		
-		dataBaseService.setPlatformuser(platformuser);
-		return new ControllerMessage(true, "Zurücksetzen erfolgreich");
+		try {
+			Platformuser platformuser = dataBaseService.getPlatformuserById(id);
+			
+			platformuser.setPassword(HashGenerator.hashPasswordWithSalt("default"));
+			
+			dataBaseService.setPlatformuser(platformuser);
+			log.info("reset password for user with id "+id);
+			return new ControllerMessage(true, "Zurücksetzen erfolgreich!");
+		} catch (Exception e)
+		{
+			log.severe("resetting password for user "+id+" failed");
+			return new ControllerMessage(false, "Zurücksetzen nicht erfolgreich!");
+		}
 	}
 
 	@RequestMapping(value = "secure/person/changeCurrentUserPassword")
@@ -190,21 +210,31 @@ public class PersonController {
 		if (currentId == null) {
 			return new ControllerMessage(false, "Nicht eingeloggt");
 		} else {
-			Platformuser platformuser = dataBaseService
-					.getPlatformuserById((int) currentId);
-			if (platformuser == null)
+			try {
+				Platformuser platformuser = dataBaseService
+						.getPlatformuserById((int) currentId);
+				if (platformuser == null)
+				{
+					log.info("user does not exist");
+					return new ControllerMessage(false, "User existiert nicht!");
+				}
+				
+				if(!HashGenerator.comparePasswordWithHash(changePasswordObject.getOldPassword(),platformuser.getPassword()))
+				{
+					log.info("old password incorrect");
+					return new ControllerMessage(false, "Altes Passwort falsch!");
+				}
+				
+				platformuser.setPassword(HashGenerator.hashPasswordWithSalt(changePasswordObject.getNewPassword()));
+				dataBaseService.setPlatformuser(platformuser);
+				log.info("changed password successfully");
+				return new ControllerMessage(true, "Ändern erfolgreich!");
+			} catch (Exception e)
 			{
-				return new ControllerMessage(false, "User existiert nicht!");
+				log.severe("changing the password failed");
+				return new ControllerMessage(false, "Ändern fehlgeschlagen!");
 			}
 			
-			if(!HashGenerator.comparePasswordWithHash(changePasswordObject.getOldPassword(),platformuser.getPassword()))
-			{
-				return new ControllerMessage(false, "Altes Passwort falsch!");
-			}
-			
-			platformuser.setPassword(HashGenerator.hashPasswordWithSalt(changePasswordObject.getNewPassword()));
-			dataBaseService.setPlatformuser(platformuser);
-			return new ControllerMessage(true, "Ändern erfolgreich!");
 		}
 	}
 
@@ -215,8 +245,7 @@ public class PersonController {
 		response.setContentType("text/csv");
 		// creates mock data
 		String headerKey = "Content-Disposition";
-		String headerValue = String.format("attachment; filename=\"%s\"",
-				csvFileName);
+		String headerValue = String.format("attachment; filename=\"%s\"", csvFileName);
 
 		response.setHeader(headerKey, headerValue);
 		List<Person> listPersons = dataBaseService.getAllPersons();
@@ -240,6 +269,8 @@ public class PersonController {
 			csvWriter.write(PersonMapper.mapToDTO(p), objectHeader);
 		}
 
+		log.info("returning CSV Export of Persons");
+		
 		csvWriter.close();
 	}
 
