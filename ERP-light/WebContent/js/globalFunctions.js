1dfbdb5b90603c0612ca82294d33f5c14fa52052
Matthias Schnöll <mschnoell.its-b2012@fh-salzburg.ac.at>
2015-04-09 12:27:49 +0200

    Fixed alert message for changing the password

--- ERP-light/WebContent/js/globalFunctions.js
+++ ERP-light/WebContent/js/globalFunctions.js
@@ -1,31 +1,31 @@
 function showAlertElement(success, text, timeOut)
 {
-		$('.myAlert').remove();
+	$('.myAlert').remove();
+
+	var successDiv = "";
+	var alertClass = "";
+
+	//set class
+	if (success == 1 || success == true)
+	{
+		alertClass = "alert alert-success";
+	}
+	else if (success == 2 || success == false)
+	{
+		alertClass = "alert alert-danger";
+	}
 	
-		var successDiv = "";
-		var alertClass = "";
+	successDiv = "<div class='"+alertClass+" myAlert' role='alert'>"+text+"</div>";
 	
-		//set class
-		if (success == 1 || success == true)
-		{
-			alertClass = "alert alert-success";
+	var domElement = $.parseHTML(successDiv);
+		
+	$("body").append(domElement);
+	
+	$('.myAlert').fadeIn("slow").delay(timeOut).fadeOut("slow",
+		function() {
+			$('.myAlert').remove();
 		}
-		else if (success == 2 || success == false)
-		{
-			alertClass = "alert alert-danger";
-		}
-		
-		successDiv = "<div class='"+alertClass+" myAlert' role='alert'>"+text+"</div>";
-		
-		var domElement = $.parseHTML(successDiv);
-			
-		$("body").append(domElement);
-		
-		$('.myAlert').fadeIn("slow").delay(timeOut).fadeOut("slow",
-			function() {
-				$('.myAlert').remove();
-			}
-		);
+	);
 }
 
 /* shows the loading spinner on the site; status: false => hide; status: true => show */
--- ERP-light/WebContent/js/header.js
+++ ERP-light/WebContent/js/header.js
@@ -42,36 +42,32 @@ $("#pageheader").load("../partials/header.html", function() {
 	// function for showing an alert message
 	function showAlertElement(success, text, timeOut)
 	{
-			$('.myAlert').remove();
+		$('.myAlert').remove();
+
+		var successDiv = "";
+		var alertClass = "";
+
+		//set class
+		if (success == 1 || success == true)
+		{
+			alertClass = "alert alert-success";
+		}
+		else if (success == 2 || success == false)
+		{
+			alertClass = "alert alert-danger";
+		}
 		
-			var successDiv = "";
-			var alertClass = "";
+		successDiv = "<div class='"+alertClass+" myAlert' role='alert'>"+text+"</div>";
 		
-			//set class
-			if (success == 1 || success == true)
-			{
-				alertClass = "alert alert-success";
+		var domElement = $.parseHTML(successDiv);
+			
+		$("body").append(domElement);
+		
+		$('.myAlert').fadeIn("slow").delay(timeOut).fadeOut("slow",
+			function() {
+				$('.myAlert').remove();
 			}
-			else if (success == 2 || success == false)
-			{
-				alertClass = "alert alert-danger";
-			}
-			
-			successDiv = "	<div class='myAlert' style='position: absolute; width: 100%; z-index: 10; top: 60px;'> " +
-							 " <div style='width: 220px; margin: auto; text-align: center; padding-top:10px;'>" +
-							 " <div class='row'> " + 
-							 " <div class='"+alertClass+"' role='alert'>"+text+"</div>" +
-							 " </div>	</div>	<div>";
-			
-			var domElement = $.parseHTML(successDiv);
-				
-			$("body").append(domElement);
-			
-			$('.myAlert').fadeIn("slow").delay(timeOut).fadeOut("slow",
-				function() {
-					$('.myAlert').remove();
-				}
-			);
+		);
 	}
 	
 	
